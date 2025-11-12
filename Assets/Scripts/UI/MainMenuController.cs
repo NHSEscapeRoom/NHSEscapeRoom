af834ec2a4b5aa56585f0138c0431ce0684ec20d
Dylan Rumble-Smith (dylan.at) <d.rs@dylan.at>
2025-11-12 15:34:17 +0000

    Add Credits Scene and create a function to load that scene when the Credits button is clicked.

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    public void GotoCreditsScreen()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
     public void QuitGame()
     {
         Application.Quit();
