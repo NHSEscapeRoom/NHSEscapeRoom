11c5d7ffc7204110f2fa6d89f7147be23f0721ab
Dylan Rumble-Smith <d.rs@dylan.at>
2026-03-05 10:23:39 +0000

    Add function summaries

--- Assets/Scripts/Main Menu/Main Menu Actions.cs
+++ Assets/Scripts/Main Menu/Main Menu Actions.cs
@@ -2,6 +2,10 @@ using UnityEngine;
 
 public class MainMenuActions : MonoBehaviour
 {
+    /// <summary>
+    /// Quits the game (with a successful state)...
+    /// This is for the quit game button(s) on the main menu.
+    /// </summary>
     public void QuitGame()
     {
         Application.Quit();
